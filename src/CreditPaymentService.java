5a6cfbfef0db21c82137932a8264a72328815aa0
Anna Litvinova <litvinova4815@gmail.com>
2022-05-24 23:42:38 +0300

    Added new files

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,15 @@
 public class CreditPaymentService {
+    public double calculate(double sum, double percent, double months) { // Формула sum*(ПС/1-(1+ПС)^months)
+
+        double result;
+        double pc = percent / (100 * 12); // месячная процентная ставка
+        double a = 1 + pc; // выражение 1+ПС
+        double b = months; // степень
+        double c = Math.pow(a, -b); // (1+ПС)^months
+        double d = 1 - c; // выражение 1-с
+        double e = pc / d; // выражение (pc/(1-c))
+        result = sum * (pc / (1 - c));
+
+        return result;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        double a = 1000000;
+        double b = 9.99;
+        double c = 12;
+        double CPS = service.calculate(a, b, c);
+        System.out.println(CPS);
+    }
 }
